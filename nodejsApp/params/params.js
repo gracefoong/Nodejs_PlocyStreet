f1efd4f49f643b564c22c90ab166beb7f3e236b4
gracefoong <77376811+gracefoong@users.noreply.github.com>
2021-05-10 14:43:00 +0800

    Add files via upload

--- nodejsApp/params/params.js
+++ nodejsApp/params/params.js
@@ -1,1 +1,5 @@
+const DATABASECONNECTION = "mongodb+srv://dbNodejs:DBnod3j2@nodejscluster.pxss0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
+module.exports = {
+    DATABASECONNECTION 
+}
